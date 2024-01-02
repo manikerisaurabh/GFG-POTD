709465b5f7f3f1ec46a6614b70c883a74faa99ca
manikerisaurabh <manikerisaurabh@gmail.com>
2024-01-02 21:50:04 +0530

    02-Jan :+1:

---
+++ 2024/Jan-2024/02-Jan.java
@@ -0,0 +1,24 @@
+public  class 02-Jan {
+
+    public long maxSumWithK(long a[], long n, long k)
+    {
+         long ans=Integer.MIN_VALUE,sum1=0,sum2=0;
+        int j=0;
+        for(int i=0;i<n;i++){
+            sum1+=a[i];
+            if(i-j+1==k){
+                ans=Math.max(sum1,ans);
+            }
+            else if(i-j+1>k){
+                sum2+=a[j++];
+                if(sum2<0){
+                    sum1-=sum2;
+                    sum2=0;
+                }
+                ans=Math.max(ans,sum1);
+            }
+        }
+        return ans;
+    }
+    
+}
