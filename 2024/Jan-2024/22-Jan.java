65e9fe65937570b7231ba6bd917c8b824f373666
manikerisaurabh <manikerisaurabh@gmail.com>
2024-01-22 20:04:03 +0530

    22-Jan :+1: Jay Shree Ram &#x1F64F;

---
+++ 2024/Jan-2024/22-Jan.java
@@ -0,0 +1,20 @@
+public class 22-Jan {
+
+    private static void helper(Node root, int target, int sum, ArrayList<ArrayList<Integer>> list, ArrayList<Integer> curr){
+        if(root == null) return;
+        curr.add(root.data);
+        sum += root.data;
+        if(sum == target) list.add(new ArrayList<>(curr));
+        helper(root.left, target, sum, list, curr);
+        helper(root.right, target, sum, list, curr);
+        curr.remove(curr.size()-1);
+        
+    }
+    public static ArrayList<ArrayList<Integer>> printPaths(Node root, int sum)
+    {
+        // code here
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        helper(root, sum, 0, list, new ArrayList<Integer>());
+        return list;
+    }
+}
