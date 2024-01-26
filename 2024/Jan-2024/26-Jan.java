47e73abf428c32d0b789a9c4949978e91771a093
manikerisaurabh <manikerisaurabh@gmail.com>
2024-01-26 22:08:47 +0530

    26-Jan :+1:  -- Happy Republic Day--

---
+++ 2024/Jan-2024/26-Jan.java
@@ -0,0 +1,22 @@
+public class 26-Jan {
+
+    //Function to get the maximum total value in the knapsack.
+    double fractionalKnapsack(int W, Item arr[], int n) 
+    {
+        // Your code here
+        Arrays.sort(arr,(a,b)->Double.compare(b.value*1.0/b.weight,a.value*1.0/a.weight));//double compare is imp for correct ans
+        
+        double ans = 0;
+        int i=0;
+        while(i<n && arr[i].weight<=W){
+            ans += arr[i].value;
+            W -= arr[i].weight;
+            i++;
+        }
+        
+        if(i<n && W>0)
+        ans += (arr[i].value*1.0/arr[i].weight) * W;
+            
+        return ans;
+    }
+}
