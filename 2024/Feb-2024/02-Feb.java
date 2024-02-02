9c6a895e8f3e403581112dcb3b74185a9e7debe5
manikerisaurabh <manikerisaurabh@gmail.com>
2024-02-02 22:24:02 +0530

    02-Feb :+1:

---
+++ 2024/Feb-2024/02-Feb.java
@@ -0,0 +1,14 @@
+public class 02-Feb {
+
+    int atoi(String s) {
+        boolean minus = s.charAt(0) == '-' ? true : false;
+        int start = s.charAt(0) == '-' ? 1 : 0;
+        int result = 0;
+        for(int i = start ; i<s.length() ; i++) {
+             if(s.charAt(i) + '0' > 115 || s.charAt(s.length()-1) == '-') return -1;
+                     int val = ('0' - ('a' - s.charAt(i))) + 1;
+                     result = result * 10 + val;
+         }
+    return minus == true ? (-1 * result ) : result ;
+}
+}
