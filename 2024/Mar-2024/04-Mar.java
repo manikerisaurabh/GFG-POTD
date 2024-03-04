66bdce73f2dd48889a319e222a39840793b7a084
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-04 20:06:23 +0530

    04-Mar :+1:

---
+++ 2024/Mar-2024/04-Mar.java
@@ -0,0 +1,12 @@
+public class 04-Mar {
+
+    public void swapElements(int[] arr, int n) {
+        // Code here
+        int temp=0;
+        for(int i=0;i+2<n;i++) { 
+            temp=arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+        }
+    }
+}
