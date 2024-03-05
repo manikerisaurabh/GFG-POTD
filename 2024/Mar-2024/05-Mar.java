64c2cf3b880b4aedca72ca35236aa56ccdc42318
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-05 22:05:00 +0530

    05-Mar :+1:

---
+++ 2024/Mar-2024/05-Mar.java
@@ -0,0 +1,36 @@
+public class 05-Mar {
+
+    static int maxIndexDiff(int a[], int n) { 
+     // Your code here
+     Stack<Integer> st=new Stack<>();
+      st.push(n-1);
+      
+      for(int i=n-2;i>=0;i--)
+      {
+          if(a[i]>a[st.peek()])
+          {
+              st.push(i);
+          }
+      }
+      
+      int low=0;
+      int ans=0;
+      
+      
+      while(!st.isEmpty() && low<n)
+      {
+          if(a[st.peek()]>=a[low])
+          {
+              ans=Math.max(ans,st.pop()-low);
+          }
+          
+          else
+          {
+              low++;
+          }
+      }
+      
+      return ans;
+      
+  }
+}
