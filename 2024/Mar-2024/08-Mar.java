8627dcd5076cc6f7ae0eb9ad364f22f41254765b
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-08 13:25:00 +0530

    08-Mar :+1:

---
+++ 2024/Mar-2024/08-Mar.java
@@ -0,0 +1,28 @@
+public class 08-Mar {
+
+    boolean sameFreq(String s) {
+        // code here
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        Map<Integer,Integer> hashMap=new HashMap<>();
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(Map.Entry<Character,Integer> m:map.entrySet()){
+            min=Math.min(m.getValue(),min);
+            max=Math.max(m.getValue(),max);
+            hashMap.put(m.getValue(),hashMap.getOrDefault(m.getValue(),0)+1);
+        }
+        if(hashMap.size()>2)
+        return false;
+        if(max>min && hashMap.get(max)>1 && min==1)
+        return true;
+        if(max>min && hashMap.get(max)>1 && min>1)
+        return false;
+        if(min==max || max==min+1)
+        return true;
+        return false;
+    }
+}
