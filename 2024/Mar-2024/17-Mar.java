6a1f95642780b7075117e279aa58f50d01d68eb4
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-17 23:12:20 +0530

    17-Mar :+1:

---
+++ 2024/Mar-2024/17-Mar.java
@@ -0,0 +1,14 @@
+public class 17-Mar {
+
+    public static int countPairs(LinkedList<Integer> head1, LinkedList<Integer> head2,int x)
+    {
+        int count=0;
+        HashSet<Integer> temp = new HashSet<>(head1);
+        for(Integer nums : head2){
+          if(temp.contains(x-nums)){
+              count++;
+          }
+        }
+        return count;
+    }
+}
