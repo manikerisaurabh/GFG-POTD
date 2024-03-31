bb22f76965003641d57ed291ee6b83334b5d6600
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-31 22:27:36 +0530

    31-Mar :+1:

---
+++ 2024/Mar-2024/31-Mar.java
@@ -0,0 +1,23 @@
+public class 31-Mar {
+
+    static int max=-1;
+    
+    public static int findMaxForN(Node root, int n) {
+       
+        traverse(root,n);
+        int ans = max;
+        max = -1;
+        return ans;
+    }
+    public static void traverse(Node root,int n){
+         if(root==null)
+            return;
+            
+        traverse(root.left,n);
+        if(root.key>max&&root.key<=n){
+            max = root.key;
+        }
+        traverse(root.right,n);
+        
+    }
+}
