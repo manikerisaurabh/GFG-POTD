27974c9b183453b05b58f04b66090ecb84334159
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-12 21:01:38 +0530

    12-Apr :+1:

---
+++ 2024/Apr-2024/12-Apr.java
@@ -0,0 +1,17 @@
+public class 12-Apr {
+    static long pairAndSum(int n, long arr[]) {
+        // code here
+          long ans = 0;
+        for (int i = 0; i < 32; i++) {
+            long c = 0;
+            long t = 1 << i;
+            for (int j = 0; j < n; j++) {
+                if ((t & arr[j]) != 0) {
+                    c++; // no.of elements that have 1's on the same bit
+                }
+            }
+            ans += (c * (c - 1) / 2) * (t); // nC2 and converting into decimal also
+        }
+        return ans;
+    }
+}
