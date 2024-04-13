ef80442047b96793f6b47ffd9ab69d7bbb40d8f7
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-13 23:27:32 +0530

    13-Apr :+1:

---
+++ 2024/Apr-2024/13-Apr.java
@@ -0,0 +1,16 @@
+public class 13-Apr {
+
+    static Long reversedBits(Long x) {
+        // code here
+          long res = 0;
+      int n = 32;
+        for (int i = 0; i < n; i++) {
+            res <<= 1;
+            if ((x & 1) == 1) {
+                res ^= 1;
+            }
+            x >>= 1;
+        }
+        return res;
+    }
+}
