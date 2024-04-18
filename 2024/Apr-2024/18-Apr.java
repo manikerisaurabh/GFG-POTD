5d552653031be832f3ab24e5df5009b085c73b77
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-18 18:46:07 +0530

    18-Apr :+1:

---
+++ 2024/Apr-2024/18-Apr.java
@@ -0,0 +1,17 @@
+public class 18-Apr {
+    public int[] twoRepeated(int arr[], int n)
+    {
+        // Your code here
+        int ans[] = new int[2];
+        int j = 0;
+        for(int i = 0; i < n+2 ; i++){
+            int val = Math.abs(arr[i]);
+            if(arr[val] < 0){
+                ans[j++] = val;
+            }else{
+                arr[val] = -arr[val];
+            }
+        }
+        return ans;
+    }
+}
