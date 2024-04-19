e9d9d184a605eafd48db189d2427fb43965083ad
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-19 21:10:58 +0530

    19-Apr :+1:

---
+++ 2024/Apr-2024/19-Apr.java
@@ -0,0 +1,18 @@
+public class 19-Apr {
+
+    ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+    {
+        HashSet<Integer> set = new HashSet<>();
+        ArrayList<Integer> al = new ArrayList<>();
+        
+        for(int i = 0; i < m; i++){
+            set.add(b[i]);
+        }
+        for(int i = 0; i < n; i++){
+            if(!set.contains(a[i])){
+                al.add(a[i]);
+            }
+        }
+        return al;
+    }
+}
