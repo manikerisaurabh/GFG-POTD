11f0de0c0faa057c15309547a91e33fa9a9537e9
manikerisaurabh <manikerisaurabh@gmail.com>
2024-05-19 23:20:04 +0530

    19-May :+1:

---
+++ 2024/May-2024/19-May.java
@@ -0,0 +1,21 @@
+public class 19-May {
+    public static int findClosest(int n, int k, int[] arr) {
+        // code here
+         for(int i=0;i<arr.length;i++){
+            if(k<=arr[i]){
+                if(i==0)return arr[i];
+                else{
+                    if(k==arr[i])return arr[i];
+                    else{
+                        int x=Math.abs(k-arr[i]);
+                        int y=Math.abs(k-arr[i-1]);
+                        if(x>y)return arr[i-1];
+                        else if(x<=y)return arr[i];
+                    }
+                }
+            }
+            
+        }
+        return arr[n-1];
+    }
+}
