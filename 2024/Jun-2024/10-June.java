2034f3f8430a6f907375bf552446512d9652fbcd
manikerisaurabh <manikerisaurabh@gmail.com>
2024-06-10 22:28:49 +0530

    10-June :+1:

---
+++ 2024/Jun-2024/10-June.java
@@ -0,0 +1,67 @@
+public class 10-June {
+
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // code here
+         HashSet<Character> hs = new HashSet<>();
+        for(int i = 0; i < n; i++)
+        {
+            hs.add(nuts[i]);
+        }
+        
+        int j = 0;
+        if(hs.contains('!'))
+        {
+            nuts[j] = '!';
+            bolts[j] = '!';
+            j++;
+        }
+        if(hs.contains('#'))
+        {
+            nuts[j] = '#';
+            bolts[j] = '#';
+            j++;
+        }
+        if(hs.contains('$'))
+        {
+            nuts[j] = '$';
+            bolts[j] = '$';
+            j++;
+        }
+        if(hs.contains('%'))
+        {
+            nuts[j] = '%';
+            bolts[j] = '%';
+            j++;
+        }
+        if(hs.contains('&'))
+        {
+            nuts[j] = '&';
+            bolts[j] = '&';
+            j++;
+        }
+        if(hs.contains('*'))
+        {
+            nuts[j] = '*';
+            bolts[j] = '*';
+            j++;
+        }
+        if(hs.contains('?'))
+        {
+            nuts[j] = '?';
+            bolts[j] = '?';
+            j++;
+        }
+        if(hs.contains('@'))
+        {
+            nuts[j] = '@';
+            bolts[j] = '@';
+            j++;
+        }
+        if(hs.contains('^'))
+        {
+            nuts[j] = '^';
+            bolts[j] = '^';
+            j++;
+        }
+    }
+}
