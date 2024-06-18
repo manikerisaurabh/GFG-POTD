d6f391f32250eb11f3706e11752e83fd6645ec5d
manikerisaurabh <manikerisaurabh@gmail.com>
2024-06-18 09:08:18 +0530

    18-June :+1:

---
+++ 2024/Jun-2024/18-June.java
@@ -0,0 +1,12 @@
+public class 18-June {
+    int count = 0;
+        int diameterSquared = 4 * r * r;
+        for (int w = 1; w <= 2 * r; w++) {
+            for (int h = 1; h <= 2 * r; h++) {
+                if (w * w + h * h <= diameterSquared) {
+                    count++;
+                }
+            }
+        }
+        return count;
+}
