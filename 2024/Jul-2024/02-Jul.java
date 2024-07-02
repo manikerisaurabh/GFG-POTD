4c683152a479d43d677aaac61ed3e65897c08046
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-02 21:49:28 +0530

    02-July :+1:

---
+++ 2024/Jul-2024/02-Jul.java
@@ -0,0 +1,17 @@
+public class 02-Jul {
+    public boolean compute(Node root) {
+        StringBuilder combinedString = new StringBuilder();
+        for (Node current = root; current != null; current = current.next) {
+            combinedString.append(current.data);
+        }
+
+        String str = combinedString.toString();
+        int n = str.length();
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
